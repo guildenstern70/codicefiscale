49e727cebec1148f763295a83988e6e158cce91d
Alessio Saltarin <alessiosaltarin@gmail.com>
2026-01-14 16:29:05 +0100

    Added month and day

--- lib/codicefiscale.ex
+++ lib/codicefiscale.ex
@@ -10,7 +10,7 @@ defmodule Codicefiscale do
   """
   
   defp check_required_fields(person) do
-    required_keys = [:name, :surname]
+    required_keys = [:name, :surname, :birth_date]
     Enum.all?(required_keys, &Map.has_key?(person, &1)) # => true
   end
   
@@ -29,7 +29,11 @@ defmodule Codicefiscale do
       person.name
       |> get_first_consonants()
       
-    first_three <> second_three
+    year = get_year(person.birth_date)
+    month = get_month(person.birth_date)
+    date = get_day(person.birth_date, person.gender)
+      
+    first_three <> second_three <> year <> month <> date
   end
   
   def get_first_consonants(word) do
@@ -40,19 +44,53 @@ defmodule Codicefiscale do
     end
   end
   
-  def get_first_consonants_one(word) do
+  def get_year(birthdate) do
+    year = birthdate.year
+    year |> Integer.to_string() |> String.slice(2..3)
+  end
+  
+  def get_month(birthdate) do
+    month = birthdate.month
+    cond do
+      month == 1 -> "A"
+      month == 2 -> "B"
+      month == 3 -> "C"
+      month == 4 -> "D"
+      month == 5 -> "E"
+      month == 6 -> "H"
+      month == 7 -> "L"
+      month == 8 -> "M"
+      month == 9 -> "P"
+      month == 10 -> "R"
+      month == 11 -> "S"
+      month == 12 -> "T"
+    end
+  end
+  
+  def get_day(birthdate, gender) do
+    day = cond do
+      gender == :male -> birthdate.day
+      gender == :female -> birthdate.day + 40
+    end
+    cond do
+      day < 10 -> "0" <> Integer.to_string(day)
+      day >= 10 -> Integer.to_string(day)
+    end
+  end
+  
+  defp get_first_consonants_one(word) do
     upword =word
     |> String.upcase()
     upword <> "XX"
   end
   
-  def get_first_consonants_two(word) do
+  defp get_first_consonants_two(word) do
     upword =word
     |> String.upcase()
     upword <> "X"
   end
-
-  def get_first_consonants_std(word, howmany) do
+  
+  defp get_first_consonants_std(word, howmany) do
     word
     |> String.replace(" ", "")
     |> String.upcase()
--- test/codicefiscale_test.exs
+++ test/codicefiscale_test.exs
@@ -19,4 +19,18 @@ defmodule CodicefiscaleTest do
   test "Normal one consonant" do
     assert Codicefiscale.get_first_consonants("A") == "AXX"
   end
+  
+  test "Birth year" do
+    assert Codicefiscale.get_year(Date.new!(1990, 1, 1)) == "90"
+  end
+  
+  test "Birth month" do
+    assert Codicefiscale.get_month(Date.new!(1970, 8, 26)) == "M"
+  end
+  
+  test "Birth day" do
+    assert Codicefiscale.get_day(Date.new!(1970, 8, 26), :male) == "26"
+    assert Codicefiscale.get_day(Date.new!(1970, 8, 26), :female) == "66"
+  end
+  
 end
